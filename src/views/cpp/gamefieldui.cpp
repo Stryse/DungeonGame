69d1171c7eb840a9dd2ea1ec1dc9b247e718ec37
Stryse <tianarath30@gmail.com>
2020-10-03 15:22:45 +0200

    shortcuts + focus policy

--- src/views/cpp/gamefieldui.cpp
+++ src/views/cpp/gamefieldui.cpp
@@ -1,14 +1,33 @@
 #include "gamefieldui.h"
 #include "ui_gamefieldui.h"
 
+#include <QKeyEvent>
+#include <QDebug>
+
 GameFieldUI::GameFieldUI(QWidget *parent) :
     QWidget(parent),
     ui(new Ui::GameFieldUI)
 {
     ui->setupUi(this);
+    setFocus();
 }
 
 GameFieldUI::~GameFieldUI()
 {
     delete ui;
+}
+
+// Navigation keys are mapped through shortcuts
+void GameFieldUI::keyPressEvent(QKeyEvent* event)
+{
+    switch (event->key())
+    {
+        case Qt::Key::Key_Escape:
+        qDebug() << "ESCAPE pressed";
+        break;
+
+        default:
+        qDebug() << "Unhandled key press";
+        break;
+    }
 }
--- src/views/header/gamefieldui.h
+++ src/views/header/gamefieldui.h
@@ -2,21 +2,34 @@
 #define GAMEFIELDUI_H
 
 #include <QWidget>
+#include <QMap>
 
 namespace Ui {
 class GameFieldUI;
 }
+
+// TYPEDEFS
+using KeyFunction = void (*(Qt::Key));
+
+class GameModel; // TODO: remove forward declaration
 
 class GameFieldUI : public QWidget
 {
     Q_OBJECT
 
 public:
+    //CTOR
     explicit GameFieldUI(QWidget *parent = nullptr);
     ~GameFieldUI();
 
+public:
+
+protected:
+    void keyPressEvent(QKeyEvent* event);
+
 private:
     Ui::GameFieldUI *ui;
+    GameModel* game;
 };
 
 #endif // GAMEFIELDUI_H
